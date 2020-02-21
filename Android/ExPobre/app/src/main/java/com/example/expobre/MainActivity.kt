be1dacf276298d1d38dddf07131bc219f51b7a73
Mayara Pereira <mayaragomper@gmail.com>
2020-02-21 15:39:21 -0300

    update MainActivity.kt

--- Android/ExPobre/app/src/main/java/com/example/expobre/MainActivity.kt
+++ Android/ExPobre/app/src/main/java/com/example/expobre/MainActivity.kt
@@ -17,11 +17,18 @@ class MainActivity : AppCompatActivity() {
         this.tvNumeros = findViewById(R.id.tvNumeros)
         this.btNovo = findViewById(R.id.btNovoJogo)
 
-        this.btNovo.setOnClickListener( { novoJogo(it) })
+        //this.btNovo.setOnClickListener( { novoJogo(it) })
 
-        }
-
+        this.btNovo.setOnClickListener(OnClickBotao())
+    }
+    /*
         fun novoJogo(view: View?){
         this.tvNumeros.text = Megasena.getInstance().joinToString("  ")
+    }*/
+
+    inner class OnClickBotao : View.OnClickListener {
+        override fun onClick(v: View?) {
+            this@MainActivity.tvNumeros.text = Megasena.getInstance().joinToString("  ")
+        }
     }
 }
