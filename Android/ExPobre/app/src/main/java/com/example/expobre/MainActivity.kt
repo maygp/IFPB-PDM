34592bb056b96f50b909352ff75ba71b51f4fd2e
Mayara Pereira <mayaragomper@gmail.com>
2020-02-21 15:30:58 -0300

    update ExPobre

--- Android/ExPobre/app/src/main/java/com/example/expobre/MainActivity.kt
+++ Android/ExPobre/app/src/main/java/com/example/expobre/MainActivity.kt
@@ -3,19 +3,25 @@ package com.example.expobre
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var tvNumeros: TextView
+    private lateinit var tvNumeros : TextView
+    private lateinit var btNovo : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         this.tvNumeros = findViewById(R.id.tvNumeros)
-    }
+        this.btNovo = findViewById(R.id.btNovoJogo)
 
-    fun novoJogo(view: View) {
-        this.tvNumeros.text = Megasena.getInstance().joinToString(" | ")
+        this.btNovo.setOnClickListener( { novoJogo(it) })
+
+        }
+
+        fun novoJogo(view: View?){
+        this.tvNumeros.text = Megasena.getInstance().joinToString("  ")
     }
 }
