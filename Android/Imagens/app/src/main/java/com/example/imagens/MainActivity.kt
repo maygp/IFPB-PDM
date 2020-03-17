002d1c3ca46712c5464b05a0d6f7c8d11ae990e5
Mayara Pereira <mayaragomper@gmail.com>
2020-03-16 21:51:05 -0300

    update imagens

--- Android/Imagens/app/src/main/java/com/example/imagens/MainActivity.kt
+++ Android/Imagens/app/src/main/java/com/example/imagens/MainActivity.kt
@@ -2,8 +2,6 @@ package com.example.imagens
 
 import android.app.Activity
 import android.app.AlertDialog
-import android.app.Dialog
-import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
@@ -11,21 +9,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.provider.MediaStore
-import android.widget.*
-import androidx.fragment.app.DialogFragment
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import java.net.URL
-import java.util.zip.Inflater
 
 class MainActivity : AppCompatActivity() {
     private lateinit var btCamera: Button
     private lateinit var btArquivo: Button
     private lateinit var btDownload: Button
     private lateinit var ivImagem: ImageView
-    private lateinit var rgImagens : RadioGroup
-    private lateinit var ldpi: RadioButton
-    private lateinit var mdpi: RadioButton
-    private lateinit var xxxhdpi: RadioButton
-
     val CAMERA = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,24 +28,13 @@ class MainActivity : AppCompatActivity() {
         this.btCamera = findViewById(R.id.btCamera)
         this.btArquivo = findViewById(R.id.btArquivo)
         this.btDownload = findViewById(R.id.btDownload)
-        this.rgImagens = findViewById(R.id.rgImagens)
-        // this.rgImagens.setOnCheckedChangeListener(ChangeRadioGroup())
-
-        this.xxxhdpi = findViewById(R.id.rbxxxhdpi)
-
         this.ivImagem = findViewById(R.id.ivImagem)
 
-        this.btCamera.setOnClickListener{ camera() }
-        this.btArquivo.setOnClickListener{ xml() }
-        this.btDownload.setOnClickListener{ criaDialog() }
-
+        this.btCamera.setOnClickListener { camera() }
+        this.btArquivo.setOnClickListener { xml() }
+        this.btDownload.setOnClickListener { download() }
     }
 
-//    inner class ChangeRadioGroup : RadioGroup.OnCheckedChangeListener{
-//        override fun onCheckedChanged(group: RadioGroup?, checkedId: Int) {
-//            Toast.makeText(this@MainActivity, checkedId.toString(), Toast.LENGTH_SHORT).show()
-//        }
-//    }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -73,14 +55,6 @@ class MainActivity : AppCompatActivity() {
     fun xml() {
         this.ivImagem.setImageResource(R.drawable.super_mario)
     }
-    //lambda
-    val positiveBtClick = {_: DialogInterface, _: Int ->
-        if(xxxhdpi.isChecked){
-            download()
-        }else{
-            Toast.makeText(this,"Selecione xxxhdpi", Toast.LENGTH_SHORT).show()
-        }
-    }
 
     fun downloadDaImagem(url: String): Bitmap {
         URL(url).openStream().use {
@@ -91,27 +65,33 @@ class MainActivity : AppCompatActivity() {
 
     fun download() {
         val handler = Handler()
-        val url = "http://www.valeria.eti.br/sm/sm_xxxhdpi.png"
+        val imagens = arrayOf("ldpi", "mdpi", "hdpi", "xhdpi", "xxhdpi", "xxxhdpi")
+        var opcao = 0
 
-        Thread {
-            val imagem = this.downloadDaImagem(url)
-
-            handler.post {
-                this.ivImagem.setImageBitmap(imagem)
+        val janela = AlertDialog.Builder(this@MainActivity)
+        janela.setTitle("Imagens")
+        janela.setNegativeButton("Cancelar", null)
+        janela.setPositiveButton("OK") { _, _ ->
+            var url = ""
+            when (opcao) {
+                0 -> url += "http://www.valeria.eti.br/sm/sm_ldpi.png"
+                1 -> url += "http://www.valeria.eti.br/sm/sm_mdpi.png"
+                2 -> url += "http://www.valeria.eti.br/sm/sm_hdpi.png"
+                3 -> url += "http://www.valeria.eti.br/sm/sm_xhdpi.png"
+                4 -> url += "http://www.valeria.eti.br/sm/sm_xxhdpi.png"
+                5 -> url += "http://www.valeria.eti.br/sm/sm_xxxhdpi.png"
             }
-        }.start()
+
+            Thread {
+                val imagem = this.downloadDaImagem(url)
+
+                handler.post {
+                    this.ivImagem.setImageBitmap(imagem)
+                }
+            }.start()
+        }
+        janela.setSingleChoiceItems(imagens, 0) { _, i ->
+            opcao = i }
+        janela.create().show()
     }
-
-    fun criaDialog() {
-        var janelinha = AlertDialog.Builder(this)
-        var view = this.layoutInflater.inflate(R.layout.dialog, null)
-
-        janelinha.setTitle("Super Mario")
-        janelinha.setIcon(R.mipmap.ic_launcher)
-        janelinha.setView(view)
-        janelinha.setPositiveButton("OK", DialogInterface.OnClickListener(positiveBtClick))
-        janelinha.setNegativeButton("Cancelar", null)
-        janelinha.create().show()
-    }
-
 }
